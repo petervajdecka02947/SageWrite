8dd6cea86b2c623c9995b0544727d5821b2b5ff2
NLP Data scientist <75378412+petervajdecka02947@users.noreply.github.com>
2022-03-09 21:50:32 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ async def post_test(text: str):
     return model.predict(text)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0", port=5000) 
+#if __name__ == "__main__":
+#    uvicorn.run("main:app",host="0.0.0.0", port=5000) 
 
 
